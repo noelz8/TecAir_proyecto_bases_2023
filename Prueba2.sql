0da534aed07aedec25f2abd407b70cab7adf98ff
noelz8 <48070788+noelz8@users.noreply.github.com>
2023-10-18 12:42:29 -0600

    Add files via upload

---
+++ Prueba2.sql
@@ -0,0 +1,13 @@
+-- Database: UsersDB
+
+-- DROP DATABASE IF EXISTS "UsersDB";
+
+CREATE DATABASE "UsersDB"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Latin America.1252'
+    LC_CTYPE = 'Spanish_Latin America.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string NOMBRE { get; set; }
+        public string APELLIDO1 { get; set; }
+        public string APELLIDO2 { get; set; }
+        public string TELEFONO { get; set; }
+        public string EMAIL { get; set; }
+        public string UNIVERSIDAD { get; set; }
+        public string CARNET { get; set; }
+        public string ROL { get; set; }
+    }
+}
