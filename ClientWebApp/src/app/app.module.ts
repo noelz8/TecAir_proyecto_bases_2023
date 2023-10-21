d5aad3ca8d7e0846063b879354cc52078c841471
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-10-20 23:01:35 -0600

    Inputs

--- ClientWebApp/src/app/app.module.ts
+++ ClientWebApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginClienteComponent } from './login-cliente/login-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginClienteComponent
   ],
   imports: [
     BrowserModule,
