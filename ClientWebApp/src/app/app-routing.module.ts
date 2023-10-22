74ca98358e559446ec6c0df29a8a9c2d22471c65
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-10-22 12:12:48 -0600

    Api RUN

--- ClientWebApp/src/app/app-routing.module.ts
+++ ClientWebApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginClienteComponent } from './login-cliente/login-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginClienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
