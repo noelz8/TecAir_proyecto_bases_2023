0669e868616638b052f378479f032113a972c197
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-10-22 23:31:01 -0600

    Script de populación

---
+++ webApi/Scripts/script_populacion.sql
@@ -0,0 +1,94 @@
+--Script de Populación
+--Inserts de los aeropuertos disponible
+
+-- Aeropuertos de Costa Rica
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('SJO', 'San José', 'Costa Rica');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('LIR', 'Liberia', 'Costa Rica');
+
+-- Aeropuertos de Panamá
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('PTY', 'Ciudad de Panamá', 'Panamá');
+
+-- Aeropuertos de Guatemala
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('GUA', 'Ciudad de Guatemala', 'Guatemala');
+
+-- Aeropuerto de El Salvador
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('SAL', 'San Salvador', 'El Salvador');
+
+-- Aeropuertos de Honduras
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('SAP', 'San Pedro Sula', 'Honduras');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('TGU', 'Tegucigalpa', 'Honduras');
+
+-- Aeropuerto de Nicaragua
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('MGA', 'Managua', 'Nicaragua');
+
+-- Aeropuerto de Belice
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('BZE', 'Ciudad de Belice', 'Belice');
+
+-- Aeropuertos de Argentina
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('EZE', 'Buenos Aires', 'Argentina');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('AEP', 'Buenos Aires', 'Argentina');
+
+-- Aeropuertos de Brasil
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('GRU', 'São Paulo', 'Brasil');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('CGH', 'São Paulo', 'Brasil');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('GIG', 'Río de Janeiro', 'Brasil');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('SDU', 'Río de Janeiro', 'Brasil');
+
+-- Aeropuerto de Colombia
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('BOG', 'Bogotá', 'Colombia');
+
+-- Aeropuertos de México
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('MEX', 'Ciudad de México', 'México');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('CUN', 'Cancún', 'México');
+
+-- Aeropuerto de Chile
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('SCL', 'Santiago', 'Chile');
+
+-- Aeropuerto de Perú
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('LIM', 'Lima', 'Perú');
+
+-- Aeropuerto de Venezuela
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('CCS', 'Caracas', 'Venezuela');
+
+-- Aeropuertos de Ecuador
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('UIO', 'Quito', 'Ecuador');
+
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('GYE', 'Guayaquil', 'Ecuador');
+
+-- Aeropuerto de Uruguay
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('MVD', 'Montevideo', 'Uruguay');
+
+-- Aeropuerto de Paraguay
+INSERT INTO Aeropuerto (CodigoAeropuerto, Ciudad, Pais)
+VALUES ('A', 'Asunción', 'Paraguay');
